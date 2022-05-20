1d5dc7ae7e4b41450d6bd5ebdbc5d6f081509cbf
moveStar <15002518569@163.com>
2022-05-20 18:04:53 +0800

    fix:fix bug

---
+++ 02_Vue3/vue3_study/14_readonly和shallowReadonly/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <h2>readonly和shallowReadonly</h2>
+  <h3>state: {{state}}</h3>
+  <button @click="update">更新数据</button>
+</template>
+<script lang="ts">
+import { defineComponent, reactive } from 'vue'
+export default defineComponent({
+  name: 'App',
+  setup() {
+    const state = reactive({
+      name: '小樱',
+      age: 18,
+      car: {
+        name: '奔驰',
+        color: 'yellow'
+      }
+    })
+    const update =() => {
+      console.log('test');
+    }
+    return {
+      state,
+      update
+    }
+  }
+})
+</script>
--- 02_Vue3/vue3_study/src/App.vue
+++ 02_Vue3/vue3_study/src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <h2>readonly和shallowReadonly</h2>
+  <h2>toRef的使用及特点：</h2>
   <h3>state: {{state}}</h3>
+  <h3>age: {{age}}</h3>
+  <h3>money: {{money}}</h3>
+  <hr>
   <button @click="update">更新数据</button>
+
+  <hr>
+  <ChildCommVue :age="age"/>
 </template>
 <script lang="ts">
-import { defineComponent, reactive } from 'vue'
+import { defineComponent, reactive, ref, toRef } from 'vue'
+import ChildCommVue from './components/ChildComm.vue'
 export default defineComponent({
   name: 'App',
+  components: {
+    ChildCommVue
+  },
   setup() {
     const state = reactive({
-      name: '小樱',
       age: 18,
-      car: {
-        name: '奔驰',
-        color: 'yellow'
-      }
+      money: 100
     })
+    // 把响应式数据state对象中的某个属性age变成ref对象l
+    const age = toRef(state, 'age')
+    // 把响应式对象中的某个属性使用ref进行包装，变成了一个ref对象
+    const money = ref(state.money)
+    console.log(age);
+    console.log(money);
+    
+    
     const update =() => {
-      console.log('test');
+      console.log('更新数据');
+      // 父子都成功更新，页面变化
+      // state.age += 1
+      // age.value += 1
+
+      // state.money += 10  //state中money更新了  const 定义的money没更新
+      money.value += 100
     }
     return {
       state,
+      age,
+      money,
       update
     }
   }
